e1a5fa8fea1a75418e5e035587ad1733cbc36954
MohamedMokhtar18 <welleamy@gmail.com>
2022-11-01 03:22:11 +0200

    -fix the paths problems

---
+++ IS/Model/CPEVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS.Model
+{
+    public class CPEVM : INotifyPropertyChanged
+    {
+        private double _iCount;
+
+        public double ICount { get => _iCount; set
+            {
+                _iCount = value;
+                OnPropertyChanged("ICount");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
